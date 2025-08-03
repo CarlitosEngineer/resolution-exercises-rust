3735487177a29f5aa45fdcdd3c76663c748729f5
CarlitosEngineer <CarlosCalderon00110001@Outlook.com>
2025-08-03 00:49:41 -0500

    zxzxc

--- src/bin/Answers_GPT.rs
+++
@@ -1,7 +0,0 @@
-use time::{Duration, PrimitiveDateTime};
-
-// Returns a DateTime one billion seconds after start.
-pub fn after(start: PrimitiveDateTime) -> PrimitiveDateTime {
-    const GIGASECOND: i64 = 1_000_000_000;
-    start + Duration::seconds(GIGASECOND)
-}
--- src/bin/gigasecond.rs
+++ src/bin/gigasecond.rs
@@ -1,6 +1,20 @@
-use time::PrimitiveDateTime as DateTime;
+// use time::macros::datetime;
+use time::{Duration, PrimitiveDateTime};
 
 // Returns a DateTime one billion seconds after start.
-pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+pub fn after(start: PrimitiveDateTime) -> PrimitiveDateTime {
+    const GIGASECOND: i64 = 1_000_000_000; // creo una constante tantos segundos.
+    start + Duration::seconds(GIGASECOND) // sumo la variable con un objeto que vale la constante
 }
+
+// fn main() {
+//     let start: PrimitiveDateTime = datetime!(2000-01-01 00:00:00);
+//     let result: PrimitiveDateTime = after(start);
+//     println!("{}", result); // para comprobar
+// }
+
+// Bibliografia:
+// https://docs.rs/time/latest/time/index.html
+// https://docs.rs/time/latest/time/struct.PrimitiveDateTime.html
+// https://docs.rs/time/latest/time/struct.Duration.html
+// cargo run --bin gigasecond
